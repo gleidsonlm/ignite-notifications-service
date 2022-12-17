bed3bc32db924a946bc85392f1338ba06ab38615
GleidsonLM <gleidsonlm@gmail.com>
2022-12-17 14:42:09 -0300

    add: entities notifications and its content. added single config file for jest

---
+++ src/app/entities/content.ts
@@ -0,0 +1,23 @@
+export class Content {
+  private readonly content: string;
+
+  get value(): string {
+    return this.content;
+  }
+
+  private validateContentLength(content: string): boolean {
+    return content.length >= 5 && content.length <= 240;
+  }
+
+  constructor(content: string) {
+    const isContentLengthValid = this.validateContentLength(content);
+
+    if (isContentLengthValid) {
+      this.content = content;
+    } else {
+      throw new Error(
+        `Content length must be between 5 and 240 characters, but was ${content.length} characters.`,
+      );
+    }
+  }
+}
